027e4589adb3b8164dfdd5dbf7315319cd304ebb
leonguyenontario <leohpnguyen@gmail.com>
2019-10-13 18:18:52 -0400

    13102019

--- projectsC++/helloworld/helloworld.cpp
+++ projectsC++/helloworld/helloworld.cpp
@@ -6,10 +6,25 @@
 
 using namespace std;
 
+// Complete the jumpingOnClouds function below.
+int jumpingOnClouds(vector<int> c, int k)
+{
+    int count = 100;
+    int i = 0;
+    do
+    {
+        count--;
+        if (c[i] == 1)
+            count -= 2;
+        i += k;
+    } while (i < c.size());
 
+    return count;
+}
 
 int main()
 {
-    vector<int> v{5, 2, 1, 3, 4};
+    vector<int> v{0 ,0, 1, 0, 0, 1, 1, 0};
+    cout<<jumpingOnClouds(v,2)<<endl;
     return 0;
 }
