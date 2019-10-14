5b11cde4c97a394076323a0441a2b49319ea5498
leonguyenontario <leohpnguyen@gmail.com>
2019-10-14 00:03:31 -0400

    14102019

--- projectsC++/helloworld/helloworld.cpp
+++ projectsC++/helloworld/helloworld.cpp
@@ -22,9 +22,42 @@ int jumpingOnClouds(vector<int> c, int k)
     return count;
 }
 
+// Complete the appendAndDelete function below.
+string appendAndDelete(string s, string t, int k)
+{
+
+    int commonLength = 0;
+    int minlength;
+    s.size() >= t.size() ? minlength = t.size() : minlength = s.size();
+
+    for (int i = 0; i < minlength; i++)
+    {
+        if (s[i] == t[i])
+            commonLength++;
+        else
+            break;
+    }
+
+    if ((s.length() + t.length() - 2 * commonLength) > k)
+    {
+        return "No";
+    }
+
+    else if ((s.length() + t.length() - 2 * commonLength)  <= k )
+    {
+        return "Yes";
+    }
+
+    else
+    {
+        return "No";
+    }
+}
+
 int main()
 {
-    vector<int> v{0 ,0, 1, 0, 0, 1, 1, 0};
-    cout<<jumpingOnClouds(v,2)<<endl;
+    // vector<int> v{0, 0, 1, 0, 0, 1, 1, 0};
+    // cout << jumpingOnClouds(v, 2) << endl;
+    // cout<<appendAndDelete("abc","abc",7);
     return 0;
 }
