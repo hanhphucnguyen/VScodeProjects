ac426c6ad09ee741fed1dad129ea413e9a7000af
leonguyenontario <leohpnguyen@gmail.com>
2019-10-16 21:16:28 -0400

    16102019

--- projectsPython/hello.py
+++ projectsPython/hello.py
@@ -2,3 +2,11 @@ import platform
 a: int = 9
 print('this is version {}'.format(platform.python_version()))
 print('lalala {}'.format(a))
+x = 6
+y = 8
+if x>y:
+    print('gbdfhgfd')
+else:
+    print('hgfhfg')
+    print('gfh')
+    print('het')
