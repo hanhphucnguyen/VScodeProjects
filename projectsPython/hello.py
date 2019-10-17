93c76709295ebed33bb99283df70e7756022316b
leonguyenontario <leohpnguyen@gmail.com>
2019-10-17 00:40:28 -0400

    17102019

--- projectsPython/hello.py
+++ projectsPython/hello.py
@@ -1,12 +1,16 @@
-import platform
-a: int = 9
-print('this is version {}'.format(platform.python_version()))
-print('lalala {}'.format(a))
+# import platform
+# a: int = 9
+# print('this is version {}'.format(platform.python_version()))
+# print('lalala {}'.format(a))
 x = 6
 y = 8
-if x>y:
-    print('gbdfhgfd')
+if x > y:
+    print('gfgg')
+elif x == y:
+    print('ffdgfd')
+elif x == 6:
+    pass
+elif x == 8:
+    pass
 else:
-    print('hgfhfg')
-    print('gfh')
-    print('het')
+    print('gfhgdf')
