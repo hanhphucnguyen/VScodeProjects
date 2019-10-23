f30ec9000dfd4e560f937d17b9e7291da851ba0a
leonguyenontario <leohpnguyen@gmail.com>
2019-10-23 14:43:50 -0400

    23102019

--- projectsPython/hello.py
+++ projectsPython/hello.py
@@ -2,12 +2,15 @@
 # a: int = 9
 # print('this is version {}'.format(platform.python_version()))
 # print('lalala {}'.format(a))
-x = 6
-y = 8
-z =[1,2,3,4,5,6]
-i = 'ads'
 
-for i in z:
-    print(i)
-i=9
-print(i)
+def main():
+    x=5
+    kitten(x)
+    print(f'x now have value: {x}')
+
+def kitten(a):
+    a=3
+    print(a)
+
+if __name__ == "__main__":
+    main()
