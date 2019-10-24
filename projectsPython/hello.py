4e9c24dc8bb3506405f9080ecde9ecd9ba56261f
leonguyenontario <leohpnguyen@gmail.com>
2019-10-24 12:41:23 -0400

    2410201

--- projectsPython/hello.py
+++ projectsPython/hello.py
@@ -4,18 +4,12 @@
 # print('lalala {}'.format(a))
 
 def main():  
-    f3()
-    
-def f(ff):
-    def wrap():
-        print('this is before f') 
-        ff()
-        print('this is after f')
-    return wrap
+    a=dict(lala=1,b=2)
+    for i in a.values():
+        print(f'{i} ')
 
-@f
-def f3():
-    print('this is f3')
+    print(a.get('lal'))
+
 
 if __name__ == "__main__":
     main()
