005b57178fb9e7fc8b169008060b5b95d26aef2f
leonguyenontario <leohpnguyen@gmail.com>
2019-10-26 23:00:55 -0400

    26102019

--- projectsPython/hello.py
+++ projectsPython/hello.py
@@ -2,30 +2,16 @@
 # a: int = 9
 # print('this is version {}'.format(platform.python_version()))
 # print('lalala {}'.format(a))
-
-
-class Car:
-    def __init__(self, *a):
-        self.a = a[0]
-        self.b = a[1]
-        self.c: int = 9
-
-    def setA(self, a=None):
-        if a:
-            self.a = a
-
-    def __str__(self):
-        return f'this is to string method {self.a} {self.b}'
-
-
 def main():
-    file1 = open('text.txt', 'rt')
-    file2 = open('textcopy.txt', 'wt')
-    for i in file1:
-        print(i.rstrip(), file=file2)
-    file2.close()
-    print('done')
-
+    print('hello world')
+    p= open('text.txt', 'rt')
+    q = open('textcopy.txt', 'wt')
+    # for i in p:
+    #     q.writelines(i)
+    while True:
+        buffer = p.read(500)
+        if buffer: q.write(buffer)
+        else: break
 
 if __name__ == "__main__":
     main()
