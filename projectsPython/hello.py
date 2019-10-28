65146c78be341cde20a1c67b1838adf9cb27183a
leonguyenontario <leohpnguyen@gmail.com>
2019-10-27 22:54:33 -0400

    27102019

--- projectsPython/hello.py
+++ projectsPython/hello.py
@@ -4,14 +4,14 @@
 # print('lalala {}'.format(a))
 def main():
     print('hello world')
-    p= open('text.txt', 'rt')
-    q = open('textcopy.txt', 'wt')
+    # p= open('text.txt', 'rt')
+    # q = open('textcopy.txt', 'wt')
     # for i in p:
     #     q.writelines(i)
-    while True:
-        buffer = p.read(500)
-        if buffer: q.write(buffer)
-        else: break
+    # while True:
+    #     buffer = p.read(500)
+    #     if buffer: q.write(buffer)
+    #     else: break
 
 if __name__ == "__main__":
     main()
