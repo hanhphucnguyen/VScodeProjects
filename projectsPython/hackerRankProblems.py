87a58c50adb9ac473bc8a02eb46648268ea69ff3
leonguyenontario <leohpnguyen@gmail.com>
2019-10-31 17:54:59 -0400

    31102019

---
+++ projectsPython/hackerRankProblems.py
@@ -0,0 +1,25 @@
+
+
+# Complete the workbook function below.
+def workbook(n, k, arr):
+    curPage=1
+    count=0
+    if n==1 and k==1:
+        return arr[0] 
+    else:       
+        for i in arr:
+            for curProb in range(1,i+1):
+                if curProb > k and curProb % k==1:
+                    curPage +=1
+                if curProb==curPage: 
+                    count +=1
+            curPage +=1
+        
+        return count
+    
+
+def main():
+    print(workbook(5,3,[4,2,6,1,10]))
+
+if __name__ == "__main__":
+    main()
