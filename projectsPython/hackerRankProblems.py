5483221dbe5727fb433f40b23bb058d85a8533a5
leonguyenontario <leohpnguyen@gmail.com>
2019-11-01 18:19:56 -0400

    1112019

--- projectsPython/hackerRankProblems.py
+++ projectsPython/hackerRankProblems.py
@@ -15,51 +15,29 @@ def workbook(n, k, arr):
 
         return count
 
-# Complete the flatlandSpaceStations function below.
-# def flatlandSpaceStations(n, c):
-#     result=0
-#     for i in range(n):
-#         min = 0
-#         temp=0
-#         if i not in c:
-#             for j in c:
-#                 if j == c[0]:
-#                     min = abs(i-j)
-#                 else:
-#                     temp = abs(i-j)
-#                     if  min>temp:
-#                         min=temp
-#         if result < min:
-#             result=min
-
-#     return  result
-
-
 def flatlandSpaceStations(n, c):
-   
+
     if len(c) == n:
-        return 0 
+        return 0
     else:
         result = 0
         min = 0
         c.sort()
         l = abs(c[0]-0)
-        r = abs(c[len(c)-1]- (n-1))
+        r = abs(c[len(c)-1] - (n-1))
         for i in range(len(c)-1):
             left = c[i]
             right = c[i+1]
-            if (right-left)%2 == 0:
-                temp = (right - left) // 2
-            else:
-                temp = (right - left) // 2 + 1
-            if min<temp: min=temp
-        result = max(min,l,r)
+            temp = (right-left)//2
+            if min < temp:
+                min = temp
+        result = max(min, l, r)
         return result
 
 
 def main():
     # print(workbook(5,3,[4,2,6,1,10]))
-    print(flatlandSpaceStations(20, [13, 1, 11, 10, 6]))
+    print(flatlandSpaceStations(100, [93, 41, 91, 61, 30, 6, 25, 90, 97]))
 
 
 if __name__ == "__main__":
