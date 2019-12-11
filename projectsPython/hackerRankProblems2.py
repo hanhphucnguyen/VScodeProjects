18617bba94957296292c60a02bf30c3383a1b68b
leonguyenontario <leohpnguyen@gmail.com>
2019-12-11 13:48:12 -0500

    11122019

--- projectsPython/hackerRankProblems2.py
+++ projectsPython/hackerRankProblems2.py
@@ -1,7 +1,12 @@
+def taumBday(b, w, bc, wc, z):
+    if (bc == wc or bc+z == wc or wc+z==bc or z>=abs(bc-wc)):
+        return b*bc + w*wc
+    else:
+        pass
 
 
 def main():
-    pass
+    print(taumBday(5,9,2,3,4))
 
 
 if __name__ == "__main__":
