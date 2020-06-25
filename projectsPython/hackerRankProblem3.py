6bce9c7a411df8fc66eb8f0fa834fb811f44a742
leonguyenontario <leohpnguyen@gmail.com>
2020-06-24 23:09:48 -0400

    24062020

--- projectsPython/hackerRankProblem3.py
+++ projectsPython/hackerRankProblem3.py
@@ -53,6 +53,17 @@ def marcsCakewalk(calorie):
     for i in range(len(calorie)):
         a += calorie[i]*pow(2, i)
     return a
+
+def flippingBits(n):
+
+    # this number is (2**32)-1
+    # unsigned integers given which is 32
+    # 2**32 = 4294967296
+    # to flip is to just minus 1
+    THE_FLIPPING = 4294967295
+
+    # ^ = bitwise XOR
+    return n ^ THE_FLIPPING
     
 def main():
     # print(bigSorting([31415926535897932384626433832795,1,3,10,3,5]))
